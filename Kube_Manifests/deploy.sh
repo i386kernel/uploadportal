a95771df7098ab2a2599038bf938ed9dbca6a316
Lakshya Nanjangud <lnanjangud@lnanjangud-a01.vmware.com>
2022-07-12 18:36:45 +0530

    added deployment script

--- Kube_Manifests/deploy.sh
+++ Kube_Manifests/deploy.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+echo "creating minio namespace"
+kubectl create namespace minio
+
+echo "changing context"
+kubectl config set-context --current --namespace=minio
+
+echo "deploying minio"
 kubectl apply -f minio
 
+sleep 10s
+
+echo "creating application namespace"
+kubectl create namespace referarchapp
+kubectl config set-context --current --namespace=referarchapp
+
+echo "Deploying MySQL"
 kubectl apply -f mysql
 
+sleep 10s
+
+echo "Creating RefArchPortal"
 kubectl apply -f referarchapp
 
